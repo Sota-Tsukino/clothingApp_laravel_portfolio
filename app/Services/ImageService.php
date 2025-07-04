d0c50b40ded65960f107bd3a546958c94b467f6e
Daishi Hioki <daishihioki@hotmail.co.jp>
2025-07-04 11:54:39 +0900

    refactering delete logic to delete uploaded image files from serve

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -6,6 +6,7 @@ use Exception;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Log;
 use InterventionImage;
+use App\Models\Image;
 use Throwable;
 
 class ImageService
@@ -28,7 +29,7 @@ class ImageService
             $resizedImage = InterventionImage::make($file)->resize(800, null, function ($constraint) {
                 $constraint->aspectRatio();
                 $constraint->upsize();
-            })->encode();//encode('jpg')で形式を指定できる
+            })->encode(); //encode('jpg')で形式を指定できる
         } catch (Throwable $e) { //通常はExceptionで十分だが、Fatal Error(メモリ不足など)も捕まえるならThrowableを使う
             Log::error('画像処理エラー: ' . $e->getMessage());
             throw new Exception('画像の読み込みに失敗しました。対応形式の画像か確認してください。');
@@ -37,5 +38,18 @@ class ImageService
         // laravelのstorage:linkを前提に、下記のpathでstorage/app/public/に保存する
         Storage::put('public/' . $folderName . '/' . $fileNameToStore, $resizedImage);
         return $fileNameToStore;
+    }
+
+    public static function deleteAllUserImages($userId)
+    {
+        $images = Image::where('user_id', $userId)->get();
+
+        foreach ($images as $image) {
+            // ファイル削除（DB削除はリレーションで対応する前提）
+            $filePath = 'public/items/' . $image->file_name;
+            if (Storage::exists($filePath)) {
+                Storage::delete($filePath);
+            }
+        }
     }
 }
