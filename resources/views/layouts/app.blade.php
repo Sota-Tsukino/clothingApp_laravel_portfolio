cda60940183ef1648c83f75315f3e2b01d101169
Daishi Hioki <daishihioki@hotmail.co.jp>
2025-07-23 14:40:49 +0900

    Fixing JS file  error after npm run build

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,52 +12,55 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite([
+        @php
+            $viteFiles = array_filter([
             'resources/css/app.css',
             'resources/js/app.js',//全ページ(blade)で読み込むのでこの中で下記JSをimportしない
 
             //↓ページ別にJS読み込み
             //管理者用
-            Request::routeIs('admin.clothing-item.create') ? 'resources/js/init-item-category-select.js' : '',
-            Request::routeIs('admin.clothing-item.create') ? 'resources/js/clothing-item.js' : '',
-            Request::routeIs('admin.clothing-item.create') ? 'resources/js/init-size-checker.js' : '',
-            Request::routeIs('admin.clothing-item.edit') ? 'resources/js/init-item-category-select.js' : '',
-            Request::routeIs('admin.clothing-item.edit') ? 'resources/js/clothing-item.js' : '',
-            Request::routeIs('admin.clothing-item.edit') ? 'resources/js/init-size-checker.js' : '',
-            Request::routeIs('admin.clothing-item.show') ? 'resources/js/init-size-checker-display.js' : '',
-            Request::routeIs('admin.clothing-item.show') ? 'resources/js/init-item-image-display.js' : '',
+            Request::routeIs('admin.clothing-item.create') ? 'resources/js/init-item-category-select.js' : null,
+            Request::routeIs('admin.clothing-item.create') ? 'resources/js/clothing-item.js' : null,
+            Request::routeIs('admin.clothing-item.create') ? 'resources/js/init-size-checker.js' : null,
+            Request::routeIs('admin.clothing-item.edit') ? 'resources/js/init-item-category-select.js' : null,
+            Request::routeIs('admin.clothing-item.edit') ? 'resources/js/clothing-item.js' : null,
+            Request::routeIs('admin.clothing-item.edit') ? 'resources/js/init-size-checker.js' : null,
+            Request::routeIs('admin.clothing-item.show') ? 'resources/js/init-size-checker-display.js' : null,
+            Request::routeIs('admin.clothing-item.show') ? 'resources/js/init-item-image-display.js' : null,
 
-            Request::routeIs('admin.sizechecker.index') ? 'resources/js/init-size-checker.js' : '',
+            Request::routeIs('admin.sizechecker.index') ? 'resources/js/init-size-checker.js' : null,
 
-            Request::routeIs('admin.profile.edit') ? 'resources/js/init-pref-city-select.js' : '',
+            Request::routeIs('admin.profile.edit') ? 'resources/js/init-pref-city-select.js' : null,
 
-            Request::routeIs('admin.measurement.edit') ? 'resources/js/calc-armpits-width.js' : '',
-            Request::routeIs('admin.measurement.create') ? 'resources/js/calc-armpits-width.js' : '',
-            Request::routeIs('admin.measurement.edit') ? 'resources/js/calc-kitake-length.js' : '',
-            Request::routeIs('admin.measurement.create') ? 'resources/js/calc-kitake-length.js' : '',
+            Request::routeIs('admin.measurement.edit') ? 'resources/js/calc-armpits-width.js' : null,
+            Request::routeIs('admin.measurement.create') ? 'resources/js/calc-armpits-width.js' : null,
+            Request::routeIs('admin.measurement.edit') ? 'resources/js/calc-kitake-length.js' : null,
+            Request::routeIs('admin.measurement.create') ? 'resources/js/calc-kitake-length.js' : null,
 
             //一般ユーザー用
-            Request::routeIs('clothing-item.create') ? 'resources/js/init-item-category-select.js' : '',
-            Request::routeIs('clothing-item.create') ? 'resources/js/clothing-item.js' : '',
-            Request::routeIs('clothing-item.create') ? 'resources/js/init-size-checker.js' : '',
-            Request::routeIs('clothing-item.edit') ? 'resources/js/init-item-category-select.js' : '',
-            Request::routeIs('clothing-item.edit') ? 'resources/js/clothing-item.js' : '',
-            Request::routeIs('clothing-item.edit') ? 'resources/js/init-size-checker.js' : '',
-            Request::routeIs('clothing-item.show') ? 'resources/js/init-size-checker-display.js' : '',
-            Request::routeIs('clothing-item.show') ? 'resources/js/init-item-image-display.js' : '',
+            Request::routeIs('clothing-item.create') ? 'resources/js/init-item-category-select.js' : null,
+            Request::routeIs('clothing-item.create') ? 'resources/js/clothing-item.js' : null,
+            Request::routeIs('clothing-item.create') ? 'resources/js/init-size-checker.js' : null,
+            Request::routeIs('clothing-item.edit') ? 'resources/js/init-item-category-select.js' : null,
+            Request::routeIs('clothing-item.edit') ? 'resources/js/clothing-item.js' : null,
+            Request::routeIs('clothing-item.edit') ? 'resources/js/init-size-checker.js' : null,
+            Request::routeIs('clothing-item.show') ? 'resources/js/init-size-checker-display.js' : null,
+            Request::routeIs('clothing-item.show') ? 'resources/js/init-item-image-display.js' : null,
 
-            Request::routeIs('sizechecker.index') ? 'resources/js/init-size-checker.js' : '',
+            Request::routeIs('sizechecker.index') ? 'resources/js/init-size-checker.js' : null,
 
-            Request::routeIs('profile.edit') ? 'resources/js/init-pref-city-select.js' : '',
+            Request::routeIs('profile.edit') ? 'resources/js/init-pref-city-select.js' : null,
 
-            Request::routeIs('measurement.edit') ? 'resources/js/calc-armpits-width.js' : '',
-            Request::routeIs('measurement.create') ? 'resources/js/calc-armpits-width.js' : '',
-            Request::routeIs('measurement.edit') ? 'resources/js/calc-kitake-length.js' : '',
-            Request::routeIs('measurement.create') ? 'resources/js/calc-kitake-length.js' : '',
-            ])
+            Request::routeIs('measurement.edit') ? 'resources/js/calc-armpits-width.js' : null,
+            Request::routeIs('measurement.create') ? 'resources/js/calc-armpits-width.js' : null,
+            Request::routeIs('measurement.edit') ? 'resources/js/calc-kitake-length.js' : null,
+            Request::routeIs('measurement.create') ? 'resources/js/calc-kitake-length.js' : null,
+            ]);
+        @endphp
+        @vite($viteFiles)
 
-            <!-- alpine.js -->
-            <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+        <!-- alpine.js -->
+        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,16 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+
+                // 下記JSファイルもビルド対象で追加する
+                'resources/js/init-size-checker.js',
+                'resources/js/init-item-category-select.js',
+                'resources/js/clothing-item.js',
+                'resources/js/init-size-checker-display.js',
+                'resources/js/init-pref-city-select.js',
+                'resources/js/calc-armpits-width.js',
+                'resources/js/calc-kitake-length.js',
+                'resources/js/init-item-image-display.js',
             ],
             refresh: true,
         }),
