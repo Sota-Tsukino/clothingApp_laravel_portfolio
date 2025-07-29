71b24c5d2cd2bb239fc7991dd2dcbce4595e4c98
Daishi Hioki <daishihioki@hotmail.co.jp>
2025-07-30 00:00:24 +0900

    Fixing contact form UI

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -16,7 +16,7 @@
 
 <body class="antialiased">
   <div
-    class="h-[100vh] p-6 lg:p-8 relative bg-dots-darker bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+    class="h-[100vh] p-6 lg:p-8 relative bg-dots-darker dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
     @if (Route::has('login'))
       <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
         @auth
