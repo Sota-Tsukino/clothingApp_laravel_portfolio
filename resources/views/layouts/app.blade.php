4abae9a0d02627a7879705d9b856e9e6700eea7a
Sota Tsukino <empathic.web.lab@gmail.com>
2025-08-22 23:25:45 +0900

    adding logout icon and modifying menu icon layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,7 +69,7 @@
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <div class="max-w-7xl mx-auto py-6 px-8 sm:px-12 lg:px-20">
                         {{ $header }}
                     </div>
                 </header>
